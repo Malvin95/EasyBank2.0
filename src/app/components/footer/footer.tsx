f5247e7ddeb0b05d7cdaffa2c0fabb2c2b2fbdfc
Malvin95 <Malvinharding@hotmail.co.uk>
2025-10-03 09:06:48 +0100

    chore: correct path implementation
    - NextJS update caused import issues, so an update to imports and reformatting of absolute pathing was done to improve folder structures

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,15 +1,15 @@
 import styles from './footer.module.css';
 import Image from 'next/image';
 
-import logo from '/public/images/logo.svg';
-import facebookIcon from '/public/images/icon-facebook.svg';
-import youtubeIcon from '/public/images/icon-youtube.svg';
-import twitterIcon from '/public/images/icon-twitter.svg';
-import pinterestIcon from '/public/images/icon-pinterest.svg';
-import instagramIcon from '/public/images/icon-instagram.svg';
+import logo from '@images/logo.svg';
+import facebookIcon from '@images/icon-facebook.svg';
+import youtubeIcon from '@images/icon-youtube.svg';
+import twitterIcon from '@images/icon-twitter.svg';
+import pinterestIcon from '@images/icon-pinterest.svg';
+import instagramIcon from '@images/icon-instagram.svg';
 
 import IconItem from './components/iconItem';
-import InviteButton from '../inviteButton/inviteButton';
+import InviteButton from '@components/inviteButton/inviteButton';
 
 interface iconObject {
     src: any;
