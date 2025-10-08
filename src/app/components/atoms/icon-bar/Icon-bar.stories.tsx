051c9d3eef37b8daddeac95a277639a35e879e4f
Malvin95 <Malvinharding@hotmail.co.uk>
2025-10-08 21:05:34 +0100

    [squash] filename changes

---
+++ src/app/components/atoms/icon-bar/Icon-bar.stories.tsx
@@ -0,0 +1,31 @@
+import { fn } from "storybook/internal/test";
+import IconBar from "./icon-bar";
+import { StoryObj } from "@storybook/nextjs/*";
+
+
+const meta = {
+    title: 'Atom/IconList',
+    component: IconBar,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { 
+            control: 'color',
+         },
+    },
+    args: { 
+        onClick: fn(),
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        primary: true,
+        label: 'IconList',
+    },
+};
