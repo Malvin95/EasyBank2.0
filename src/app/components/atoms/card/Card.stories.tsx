e61078057486c3b6c9beb8edbf99a46af5a889c8
Malvin95 <Malvinharding@hotmail.co.uk>
2025-10-11 18:34:53 +0100

    refactor: refactored the Card Component
    - Changed the card component so that the Icon Card and the Blog Card  can both use the same component.

---
+++ src/app/components/atoms/card/Card.stories.tsx
@@ -0,0 +1,36 @@
+import Card, { CardType } from "./Card";
+import { Meta, StoryObj } from "@storybook/nextjs/*";
+import onlineImage from '../../../../../public/images/icon-online.svg';
+
+const meta = {
+    title: 'Atom/Card',
+    component: Card,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs']
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const InfoCard: Story = {
+    args: {
+        cardType: CardType.INFO,
+        imageUrl: onlineImage,
+        title: 'Test Info Title',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique tortor et ultricies tristique.',
+        alt: 'test alt tag'
+    }
+}
+
+export const BlogCard: Story = {
+    args: {
+        cardType: CardType.BLOG,
+        imageUrl: '/images/image-currency.jpg',
+        author: 'John Doe',
+        title: 'Test Blog Title',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique tortor et ultricies tristique.',
+        alt: 'test alt tag'
+    }
+}
