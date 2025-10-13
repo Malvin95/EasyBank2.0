d290e5c74c21e2ffbe9c0fa1c053ca8ba32b31f7
Malvin G Harding <Malvinharding@hotmail.co.uk>
2025-10-13 10:56:27 +0100

    Card refactor (#21)
    * refactor: refactored the Card Component
    
    - Changed the card component so that the Icon Card and the Blog Card  can both use the same component.
    
    * [squash] fixed minor UI defects

---
+++ src/app/components/atoms/card/Card.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+
+export enum CardType {
+    INFO = 'InfoCard',
+    BLOG = 'BlobCard'
+};
+
+export type CardData = {
+    cardType: CardType,
+    imageUrl: string,
+    author?: string,
+    title: string,
+    content: string,
+    alt: string,
+}
+
+export default function Card({cardType, imageUrl, author, title, content, alt}: CardData){
+    const cardWrapper = cardType == CardType.BLOG ? "min-h-[425px]" : "min-h-[200px]";
+    const blogCardFrame = cardType == CardType.BLOG ? "rounded-sm bg-white" : "";
+    const cardContentContainer = cardType == CardType.BLOG ? "m-8" : "";
+
+    return(
+        <div className={`w-72 inline-flex mr-8 ${cardWrapper}`}>
+            <div className={`w-full ${blogCardFrame}`}>
+                {cardType === CardType.BLOG && (
+                    <div className="w-full">
+                        <Image 
+                            src={imageUrl}
+                            className="rounded-t-sm h-48"
+                            height={192}
+                            width={288}
+                            alt={alt} 
+                        /> 
+                    </div>
+                )}
+                <div className={cardContentContainer}>
+                    {cardType === CardType.INFO && (
+                        <Image 
+                            src={imageUrl}
+                            className="rounded-t-sm mb-6"
+                            height={70}
+                            width={70}
+                            alt={alt} 
+                        />
+                    )}
+                    {author && <p className="mt-1 mr-auto ml-auto mb-2.5 text-xs font-light text-gray-400">By {author}</p>}
+                    <p className="font-normal text-lg mt-3.5 mb-2 m-auto text-gray-900">{title}</p>
+                    <p className="mb-1 text-xs font-light text-gray-400">{content}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/components/organisms/whySec/WhySectionOrg.tsx
+++ src/app/components/organisms/whySec/WhySectionOrg.tsx
@@ -1,6 +1,6 @@
+import { infoContent } from '@components/atoms/card/CardContent';
 import styles from './whySection.module.css';
-
-import WhyList from '@components/atoms/infoList/InfoList';
+import Card, { CardType } from '@components/atoms/card/Card';
 
 export default function WhySection() {
     let subtext = 'We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.';
@@ -13,7 +13,16 @@ export default function WhySection() {
                     <p className={styles.subHeading}>{subtext}</p>
                 </div>
                 <div className={styles.contentContainer}>
-                    <WhyList />
+                    {infoContent.map((item, key) => (
+                        <Card
+                            cardType={CardType.INFO}
+                            imageUrl={item.imageUrl}
+                            title={item.title}
+                            content={item.content}
+                            alt={item.alt}
+                            key={key}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
