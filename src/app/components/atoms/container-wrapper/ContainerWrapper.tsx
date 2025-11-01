fa13f1776f6b89e453330ef80c4356e556933ada
Malvin95 <Malvinharding@hotmail.co.uk>
2025-11-01 12:49:01 +0000

    chore: cleaned up UI defects
    We needed to clean up some minor UI defects that appeared after the project production UI reset.

--- src/app/components/atoms/container-wrapper/ContainerWrapper.tsx
+++ src/app/components/atoms/container-wrapper/ContainerWrapper.tsx
@@ -1,13 +1,13 @@
 import { TemplateFormat } from "@components/molecules/sectionContainer/SectionContainer";
 
 
-export default function ContainerFrame({templateFormat, children}: {templateFormat?: TemplateFormat, children: React.ReactNode}) {
-    const wrapperStyles = 'h-full w-2xs mt-16 mb-14 sm:w-[580px] md:w-2xl lg:w-4xl xl:w-6xl';
+export default function ContainerWrapper({templateFormat, children}: {templateFormat?: TemplateFormat, children: React.ReactNode}) {
+    const wrapperStyles = 'h-full m-auto w-2xs sm:w-[580px] md:w-2xl lg:w-4xl xl:w-6xl';
     const wrapper = templateFormat === TemplateFormat.LATEST || TemplateFormat.WHY || undefined ? wrapperStyles : '';
     const bgColorAndzIndex = templateFormat === TemplateFormat.WHY ? 'bg-gray-100' : 'bg-zinc-50';
 
     return(
-        <div className={`flex w-full min-h-[660px] text-center justify-center xl:text-left ${bgColorAndzIndex}`}>
+        <div className={`${templateFormat ? 'min-h-[660px]' : ''} flex w-full text-center justify-center xl:text-left ${bgColorAndzIndex}`}>
             <div className={wrapper}>
                 {children}
             </div>
--- src/app/components/molecules/footer/FooterComponent.tsx
+++ src/app/components/molecules/footer/FooterComponent.tsx
@@ -7,8 +7,8 @@ import IconBar from '@components/atoms/icon-bar/IconBar';
 
 export default function Footer() {
     return(
-        <div className="flex w-full content-center justify-center bg-[hsl(233,26%,24%)] center min-h-48">  {/*Formerly footerContainer className.*/}
-            <div className='h-full w-2xs mt-auto mb-auto sm:w-[580px] md:flex md:w-2xl lg:w-4xl xl:w-6xl'> {/* New div */}
+        <div className="flex w-full content-center justify-center bg-[hsl(233,26%,24%)] center min-h-48">
+            <div className='h-full w-2xs mt-auto mb-auto sm:w-[580px] md:flex md:w-2xl lg:w-4xl xl:w-6xl'>
                 <div className="h-40 flex flex-col justify-evenly w-fit m-auto md:w-1/3">
                     <div className="w-fit">
                         <Image src={footerlogo} alt='logo'/>
