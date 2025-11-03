364ffa6b028ab4d9b769f8071ac5d2c6447047f4
Malvin95 <Malvinharding@hotmail.co.uk>
2025-11-03 15:27:20 +0000

    [squash] positioned the mobile image

--- src/app/components/atoms/container-wrapper/ContainerWrapper.tsx
+++ src/app/components/atoms/container-wrapper/ContainerWrapper.tsx
@@ -1,16 +1,17 @@
 import { TemplateFormat } from "@components/molecules/sectionContainer/SectionContainer";
 
-
 export default function ContainerWrapper({templateFormat, children}: {templateFormat?: TemplateFormat, children: React.ReactNode}) {
+
     const wrapperStyles = 'h-full m-auto w-full max-w-[375px] min-[375px]:max-w-[375px] min-[576px]:max-w-[500px] sm:max-w-[580px] md:max-w-2xl lg:max-w-4xl xl:max-w-6xl';
     const wrapper = templateFormat === TemplateFormat.LATEST || TemplateFormat.WHY || undefined ? wrapperStyles : '';
     const bgColorAndzIndex = templateFormat === TemplateFormat.WHY ? 'bg-gray-100' : 'bg-zinc-50';
+    // const backgroundStyles = templateFormat === TemplateFormat.HERO ? "bg-[url(/images/bg-intro-desktop.svg)] bg-no-repeat bg-position-[-167px_-291px] bg-size-[240%] sm:bg-position-[-160px_-410px] sm:bg-size-[200%] md:bg-position-[-220px_-527px] lg:bg-position-[475px_-70px] lg:bg-size-[110%] desktop:bg-position-[630px_-175px] desktop:bg-size-[1250px]" : '';
 
     return(
-        <div className={`${templateFormat ? 'min-h-[660px]' : ''} flex w-full text-center justify-center xl:text-left ${bgColorAndzIndex}`}>
+        <div className={`${templateFormat ? 'min-h-[660px]' : ''} flex w-full text-center justify-center xl:text-left ${bgColorAndzIndex} overflow-x-clip overflow-y-visible`}>                            
             <div className={wrapper}>
                 {children}
-            </div>
+            </div>    
         </div>
     )
 }
--- src/app/components/organisms/hero-section/HeroSectionOrg.tsx
+++ src/app/components/organisms/hero-section/HeroSectionOrg.tsx
@@ -1,19 +1,22 @@
+import Image from 'next/image';
+
+import imageMockup from '@images/image-mockups.png'; 
+// import bgImage from '@images/bg-intro-desktop.svg';
 import InviteButton from '@components/atoms/invite-button/InviteButtonComponent';
 import ContainerTemplate from '@components/atoms/container-template/ContainerTemplate';
 import { TemplateFormat } from '@components/molecules/sectionContainer/SectionContainer';
 import ContainerWrapper from '@components/atoms/container-wrapper/ContainerWrapper';
 
-
 const title = 'Next generation digital banking';
 const subtitle = 'Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.';
 
-// const mobileStyles = "min-[375px]:-top-[54px] min-[375px]:-mb-20 min-[576px]:-top-[100px] min-[576px]:-mb-32"; // Custom breakpoint styles for smaller devices
+const mobileStyles = "min-[375px]:-top-[54px] min-[375px]:-mb-20 min-[576px]:-top-[100px] min-[576px]:-mb-32"; // Custom breakpoint styles for smaller devices
 
 export function ResponsiveHeroSection() {
     return(
         <ContainerWrapper templateFormat={TemplateFormat.HERO}>
             <div className='flex flex-wrap-reverse h-full lg:flex-nowrap'>
-                <div className='flex h-[720px] lg:h-[800px] lg:w-1/2 xl:w-2/3 mt-20'>
+                <div className='flex lg:h-[750px] lg:w-1/2 xl:w-2/3'>
                     <div className='mt-auto lg:m-auto'>
                         <ContainerTemplate 
                             templateFormat={TemplateFormat.HERO} 
@@ -27,11 +30,10 @@ export function ResponsiveHeroSection() {
                     </div>
                 </div>
                 {/* Removed the styles: bg-[url(/images/bg-intro-desktop.svg)] bg-no-repeat bg-position-[30%_-350px] bg-size-[240%] */}
-                {/* 
-                    <div className='w-full lg:h-full lg:w-1/2'> 
-                        <Image src={imageMockup} alt='imgMockups' className={`relative z-1 -top-8 -mb-16 ${mobileStyles} sm:-top-32 sm:-mb-36 md:-top-40 md:-mb-48`}/>
-                    </div> 
-                */}
+                <div className='w-full lg:h-full lg:w-1/2 xl:w-1/3 z-2'> 
+                    {/* <Image src={bgImage} alt='imgMockups' className={`relative z-1 -top-8 -mb-16 ${mobileStyles} sm:-top-32 sm:-mb-36 md:-top-40 md:-mb-48 lg:top-40 lg:left-[165px] lg:scale-150 xl:scale-200 xl:left-28 xl:top-48 desktop:left-22`}/> */}
+                    <Image src={imageMockup} alt='imgMockups' className={`relative z-1 -top-8 -mb-16 ${mobileStyles} sm:-top-32 sm:-mb-36 md:-top-40 md:-mb-48 lg:top-40 lg:left-[165px] lg:scale-150 xl:scale-200 xl:left-28 xl:top-48 desktop:left-22`}/>
+                </div>
             </div>
         </ContainerWrapper>
     )
